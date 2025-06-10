4e4deb9ba9860ebe2366adaefc07fd94f01b3041
pradeepDu <dukemarquis2004@gmail.com>
2025-06-10 16:01:21 +0530

    added color scheme and sidebar

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:ai_app/widgets/side_bar.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Row(children: [
+        SideBar(),
+        Column(children: [
+
+        ],)
+      ],)
+     
+    );
+  }
+}
---
+++ lib/widgets/side_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:ai_app/theme/colors.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 69, 
+      color: AppColors.sideNav);
+
+  }
+}
