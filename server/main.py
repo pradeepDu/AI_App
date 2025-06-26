23dd3ac478c9da534be2d04bf7e891e24a289fad
pradeepDu <dukemarquis2004@gmail.com>
2025-06-26 20:28:10 +0530

    test fastapi

--- server/main.py
+++ server/main.py
@@ -1,1 +1,13 @@
-print("heelo world")
+from fastapi import FastAPI
+from typing import Dict
+
+app = FastAPI()
+
+@app.get("/")
+def read_root() -> Dict[str, str]:
+    return {"message": "Hello from FastAPI"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
+
