e446ffa357a2402ecb3438154ac109ac02f7ece4
pradeepDu <dukemarquis2004@gmail.com>
2025-06-27 20:15:05 +0530

    testing routes

--- server/main.py
+++ server/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from typing import Dict
+from pydantic_models.chat_body import ChatBody
 
 app = FastAPI()
 
-@app.get("/")
-def read_root() -> Dict[str, str]:
-    return {"message": "Hello from FastAPI"}
-
+@app.post("/chat")
+def chat_endpoint(body: ChatBody):
+    print(body.query)
+    return body.query
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000)
-
