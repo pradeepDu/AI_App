a7aa9aaa99de60e88e6d182c3c37f6728bc2a3b3
pradeepDu <dukemarquis2004@gmail.com>
2025-07-09 17:35:30 +0530

    added and tested websocket for gemini and travily

--- server/main.py
+++ server/main.py
@@ -1,18 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , WebSocket 
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic_models.chat_body import ChatBody
 from services.search_service import SearchService
 from services.sort_source_service import SortSourceService
-
+from services.llm_service import LLMService
+import asyncio
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 search_service = SearchService()
 sort_source_service =SortSourceService()
-@app.post("/chat")
-def chat_endpoint(body: ChatBody):
-    search_results=search_service.web_search(query=body.query)
-    
-    sorted_results = sort_source_service.sort_sources(body.query,search_results)
-    print(sorted_results)
-    return body.query
+llm_service = LLMService()
+
+@app.websocket("/ws/chat")
+async def websocket_chat_endpoint(websocket:WebSocket):
+    await websocket.accept()
+    try:
+         await asyncio.sleep(0.1)
+         data = await websocket.receive_json()
+         query = data.get("query", "")
+         search_results=search_service.web_search(query)
+         
+         sorted_results = sort_source_service.sort_sources(query,search_results)
+         await asyncio.sleep(0.1)
+         await websocket.send_json({"type": "search_results", "data": sorted_results}) 
+         for chunk in llm_service.generate_response(query,sorted_results):
+          await asyncio.sleep(0.1)
+          await websocket.send_json({"type": "content", "data": chunk})
+    except:
+        print("Unexpected error occurred")
+    finally: 
+        await websocket.close() 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="localhost", port=8000,reload=True)
--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -6,4 +6,20 @@ class LLMService:
         genai.configure(api_key=settings.GEMINI_API_KEY)
         self.model = genai.GenerativeModel("gemini-2.0-flash")
     def generate_response(self,query:str,search_results :list[dict]):
-        pass
+        
+        context_text = "\n\n".join(
+            [
+            f"Source {i+1} ({result['url']}):\n{result['content']}"
+            for i, result in enumerate(search_results)
+            ]
+        )
+        full_prompt = f"""
+        Context from web search:
+        {context_text}
+        Query: {query}
+        please provide a comprehensive,detailed,well-citied accurate response using the above context.Think and reason deeply . ensure it answers the query the user is asking.Do not use your knowledge until it is absolutely neccessary.
+        """
+        response = self.model.generate_content(full_prompt,stream = True)
+        for chunk in response:
+            yield chunk.text
+        
